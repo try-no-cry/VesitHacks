68d8cfc17f280f9036b37acc387e0f47de6a0404
yashgoyal24 <44055950+yashgoyal24@users.noreply.github.com>
2019-09-22 17:46:27 +0530

    Update employeemanagement.sql

--- employeemanagement.sql
+++ employeemanagement.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 22, 2019 at 12:33 PM
+-- Generation Time: Sep 22, 2019 at 02:14 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `completed_projects` (
-  `project_id` int(11) NOT NULL AUTO_INCREMENT,
+  `proj_id` int(11) NOT NULL,
   `project_name` varchar(255) NOT NULL,
   `project_status` varchar(255) NOT NULL,
   `project_manager_id` int(11) NOT NULL,
@@ -44,11 +44,11 @@ CREATE TABLE `completed_projects` (
 --
 
 CREATE TABLE `current_project` (
-  `proj_id` int(11) NOT NULL AUTO_INCREMENT,
+  `proj_id` int(11) NOT NULL,
   `proj_manager_id` int(11) NOT NULL,
   `status` varchar(255) NOT NULL,
   `task_name` varchar(255) NOT NULL,
-  `task_description` varchar(255) NOT NULL,
+  `task_description` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -60,7 +60,7 @@ CREATE TABLE `current_project` (
 --
 
 CREATE TABLE `manager_team` (
-  `team_id` int(11) NOT NULL AUTO_INCREMENT,
+  `team_id` int(11) NOT NULL,
   `manager_id` int(11) NOT NULL,
   `member_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -72,11 +72,11 @@ CREATE TABLE `manager_team` (
 --
 
 CREATE TABLE `review` (
-  `review_id` int(11) NOT NULL AUTO_INCREMENT,
+  `review_id` int(11) NOT NULL,
   `r_for_id` int(11) NOT NULL,
   `r_by_id` int(11) NOT NULL,
-  `puctuality` int(11) NOT NULL,
-  `targets_achieved` int(11) NOT NULL,
+  `punctuality` int(11) NOT NULL,
+  `targets_acheived` int(11) NOT NULL,
   `behaviour` int(11) NOT NULL,
   `contribution` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -88,16 +88,15 @@ CREATE TABLE `review` (
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `contact` varchar(10) NOT NULL,
+  `password` text NOT NULL,
+  `address` text NOT NULL,
+  `contact` varchar(11) NOT NULL,
   `current_project_id` int(11) NOT NULL,
   `salary` int(11) NOT NULL,
-  `projects_done` int(11) NOT NULL,
-  `role` int(11) NOT NULL
+  `projects_done` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -108,7 +107,7 @@ CREATE TABLE `users` (
 -- Indexes for table `completed_projects`
 --
 ALTER TABLE `completed_projects`
-  ADD PRIMARY KEY (`project_id`),
+  ADD PRIMARY KEY (`proj_id`),
   ADD KEY `project_manager_id` (`project_manager_id`);
 
 --
@@ -123,8 +122,8 @@ ALTER TABLE `current_project`
 --
 ALTER TABLE `manager_team`
   ADD PRIMARY KEY (`team_id`),
-  ADD KEY `member_id` (`member_id`),
-  ADD KEY `manager_id` (`manager_id`);
+  ADD KEY `manager_id` (`manager_id`),
+  ADD KEY `member_id` (`member_id`);
 
 --
 -- Indexes for table `review`
@@ -138,7 +137,41 @@ ALTER TABLE `review`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `completed_projects`
+--
+ALTER TABLE `completed_projects`
+  MODIFY `proj_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `current_project`
+--
+ALTER TABLE `current_project`
+  MODIFY `proj_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `manager_team`
+--
+ALTER TABLE `manager_team`
+  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `review`
+--
+ALTER TABLE `review`
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -148,27 +181,27 @@ ALTER TABLE `users`
 -- Constraints for table `completed_projects`
 --
 ALTER TABLE `completed_projects`
-  ADD CONSTRAINT `completed_projects_ibfk_1` FOREIGN KEY (`project_manager_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `completed_projects_ibfk_1` FOREIGN KEY (`project_manager_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `current_project`
 --
 ALTER TABLE `current_project`
-  ADD CONSTRAINT `current_project_ibfk_1` FOREIGN KEY (`proj_manager_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `current_project_ibfk_1` FOREIGN KEY (`proj_manager_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `manager_team`
 --
 ALTER TABLE `manager_team`
-  ADD CONSTRAINT `manager_team_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `manager_team_ibfk_2` FOREIGN KEY (`manager_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `manager_team_ibfk_1` FOREIGN KEY (`manager_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `manager_team_ibfk_2` FOREIGN KEY (`member_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `review`
 --
 ALTER TABLE `review`
-  ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`r_for_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `review_ibfk_2` FOREIGN KEY (`r_by_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`r_for_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `review_ibfk_2` FOREIGN KEY (`r_by_id`) REFERENCES `users` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
