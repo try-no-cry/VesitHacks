3e90365ebade4e879a47459c70d5aafe45e2d095
yashgoyal24 <44055950+yashgoyal24@users.noreply.github.com>
2019-09-22 13:48:58 +0530

    created table for profile

--- resources/views/profiledetails.blade.php
+++ resources/views/profiledetails.blade.php
@@ -1,6 +1,37 @@
 @extends('master')
 
 @section('content')
-<h1>Profile details</h1>
-
+<h3>Profile details</h3>
+<table class="centered" id="tableformat" style="width:100%">
+  
+  <tr>
+    <td><b>Name</b></td>
+    <td>Yash Goyal</td>
+   
+  </tr>
+   
+  <tr>
+    <td><b>Employee ID</b></td>
+    <td>1</td>
+   
+  </tr>
+   
+  <tr>
+    <td><b>Role</b></td>
+    <td>ABC</td>
+   
+  </tr>
+   
+  <tr>
+    <td><b>Current Projects</b></td>
+    <td>XYZ</td>
+   
+  </tr>
+   
+  <tr>
+    <td><b>Past Projects</b></td>
+    <td>BCDE</td>
+   
+  </tr>
+  </table>
 @endsection
