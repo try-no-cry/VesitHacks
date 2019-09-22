52773985c61653f2dec146f23b362657100af23f
unknown <prtk.mehta@gmail.com>
2019-09-22 12:04:06 +0530

    added dashboard

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/register', function () {
     return view('register') ;
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard') ;
+});
