7993886ffe45a3212bb159b5fa5f6dcc0ccc5a31
Heramb <44063354+Hearmb@users.noreply.github.com>
2019-09-22 13:42:40 +0530

    footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -143,45 +143,7 @@
                  <div class="parallax"><img src="images/background3.jpg" alt="Unsplashed background img 3"></div>
                </div>
              
-<<<<<<< HEAD
-               <footer class="page-footer teal">
-                 <div class="container">
-                   <div class="row">
-                     <div class="col l6 s12">
-                       <h5 class="white-text">EPARS</h5>
-                       <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
-             
-             
-                     </div>
-                     <div class="col l3 s12">
-                       <h5 class="white-text">Settings</h5>
-                       <ul>
-                         <li><a class="white-text" href="#!">Link 1</a></li>
-                         <li><a class="white-text" href="#!">Link 2</a></li>
-                         <li><a class="white-text" href="#!">Link 3</a></li>
-                         <li><a class="white-text" href="#!">Link 4</a></li>
-                       </ul>
-                     </div>
-                     <div class="col l3 s12">
-                       <h5 class="white-text">Connect</h5>
-                       <ul>
-                         <li><a class="white-text" href="#!">Link 1</a></li>
-                         <li><a class="white-text" href="#!">Link 2</a></li>
-                         <li><a class="white-text" href="#!">Link 3</a></li>
-                         <li><a class="white-text" href="#!">Link 4</a></li>
-                       </ul>
-                     </div>
-                   </div>
-                 </div>
-                 <div class="footer-copyright">
-                   <div class="container">
-                   Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
-                   </div>
-                 </div>
-               </footer>
-=======
-            
->>>>>>> 38c5f01dfef53ac60b5e06da437c0507e8227813
+              
              
 
 @endsection
