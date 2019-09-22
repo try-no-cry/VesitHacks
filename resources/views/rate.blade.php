f019c2cd956386b6c7920d7944c7bd44c4d7715c
unknown <prtk.mehta@gmail.com>
2019-09-22 15:27:04 +0530

    added form

--- resources/views/rate.blade.php
+++ resources/views/rate.blade.php
@@ -1,4 +1,42 @@
+<html>
+    <head>
+        <style>
+            ul {
+                display:none;
+            }
+            div:hover ul{
+                display: block;
+            }
+            
+        </style>
+    </head>
+</html>
 @extends('master')
 @section('content')
-    <p>Rate</p>
+    <ol>
+        <div class="sh">
+            <li><a href="#"><span>ONE</span></a></li>
+            <ul>
+                <li><input type="text" placeholder="one"></li>
+                <li><input type="text" placeholder="one"></li>
+                <li><input type="text" placeholder="one"></li>
+            </ul>
+        </div>
+        <div class="sh">
+            <li><a href="#"><span>ONE</span></a></li>
+            <ul>
+                <li><input type="text" placeholder="one"></li>
+                <li><input type="text" placeholder="one"></li>
+                <li><input type="text" placeholder="one"></li>
+            </ul>
+        </div>
+        <div class="sh">
+            <li><a href="#"><span>ONE</span></a></li>
+            <ul>
+                <li><input type="text" placeholder="one"></li>
+                <li><input type="text" placeholder="one"></li>
+                <li><input type="text" placeholder="one"></li>
+            </ul>
+        </div>
+    </ol>
 @endsection
