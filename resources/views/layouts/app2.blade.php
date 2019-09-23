6ec2b92f65a65ac46d28961ab1d962aa1d5a60bf
Heramb <44063354+Hearmb@users.noreply.github.com>
2019-09-23 12:34:01 +0530

    footer

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -98,7 +98,7 @@
    <footer class="page-footer" style="background-color:#3F51B5">
                  <div class="container">
                    <div class="row">
-                     <div class="col l6 s12">
+                     <div class="col m5 s12">
                        <h5 class="white-text">EPARS</h5>
                        <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
              
@@ -106,13 +106,13 @@
                      </div>
               
                 
-                     <div class="col l6 s12">
+                     <div class="col m3 offset-m3 s12">
                        <h5 class="white-text">Connect</h5>
                        <ul>
-                         <li><a class="white-text" href="#!">Link 1</a></li>
-                         <li><a class="white-text" href="#!">Link 2</a></li>
-                         <li><a class="white-text" href="#!">Link 3</a></li>
-                         <li><a class="white-text" href="#!">Link 4</a></li>
+                       <span><li><a class="white-text" href="https://facebook.com">Facebook</a></li><span>
+                         <li><a class="white-text" href="https://instagram.com">Instagram</a></li>
+                         <li><a class="white-text" href="https://twitter.com">Twitter</a></li>
+                         <li><a class="white-text" href="https://linkedin.com">LinkedIn</a></li>
                        </ul>
                      </div>
                    </div>
